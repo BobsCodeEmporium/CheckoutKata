fd33e0c17c278f8e6102c78a7d9a80d7c4443e3b
Armand Lawson <armand@lawson-software.co.uk>
2019-02-21 20:50:59 +0000

    Added a way to run in the console.

--- Checkout/Checkout/Program.cs
+++ Checkout/Checkout/Program.cs
@@ -6,7 +6,41 @@ namespace Checkout
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var checkout = new Checkout();
+            var run = true;
+
+            Console.WriteLine("SKU  Price  Offer");
+            Console.WriteLine(" A      50  3 for 130");
+            Console.WriteLine(" B      30  2 for 45");
+            Console.WriteLine(" C      20");
+            Console.WriteLine(" D      15");
+            Console.WriteLine();
+            Console.WriteLine("Press T for total.");
+
+            do
+            {
+                Console.WriteLine();
+
+                var action = Console.ReadLine();
+                var actionUpper = action?.ToUpper();
+
+                switch (actionUpper)
+                {
+                    case "A":
+                    case "B":
+                    case "C":
+                    case "D":
+                        checkout.Scan(actionUpper);
+                        break;
+                    case "T":
+                        Console.WriteLine($"Your total is: £{checkout.GetTotalPrice()}");
+                        break;
+                    case "X":
+                        run = false;
+                        break;
+                }
+
+            } while (run);
         }
     }
 }
